9611fae639a2236ea92c94193e7e248b1b3981ca
Sagasta46 <shiva3sundar@gmail.com>
2025-02-25 15:55:47 +0530

    first commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,35 @@
+import { CONTACT } from "../constants"
+import { motion } from "framer-motion"
+
+
+const Contact = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-2">
+        <motion.h1
+        whileInView={{opacity:1,y:0}}
+        initial={{opacity : 0, y:-100}}
+        transition={{duration: 0.5}} className="my-8 text-center text-4xl">Get in Touch !</motion.h1>
+        <div className="text-center tracking-tighter">
+            <motion.p 
+            initial={{x:100 ,opacity: 0}}
+            animate={{x:0, opacity:1}} 
+            transition={{duration: 1}}
+            className="my-4">{CONTACT.address}</motion.p>
+            <motion.p
+            initial={{x: -100 ,opacity: 0}}
+            animate={{x:0, opacity:1}} 
+            transition={{duration: 1}}
+            
+            className="my-4">{CONTACT.phoneNo}</motion.p>
+            <motion.a
+            initial={{x: 100 ,opacity: 0}}
+            animate={{x:0, opacity:1}} 
+            transition={{duration: 1}}
+           
+            href="#" className="border-b">{CONTACT.email}</motion.a>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/constants/index.js
@@ -0,0 +1,85 @@
+/* import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg"; */
+
+export const HERO_CONTENT = `I am a passionate full-stack developer with a knack for crafting optimized and scalable web applications. With 3 years of hands-on experience, I have honed my skills in front-end technologies like JavaScript and React.js, as well as back-end technologies like Node.js, Express.js, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience in the field. Over the past 3 years, I have gained extensive experience working with React.js, JavaScript, HTML5, and CSS3, and have recently upskilled myself in the MERN stack, gaining strong knowledge of Node.js, Express.js, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2021 - 2024",
+    role: "Front-End Developer",
+    company: "Tech Mahindra.",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "HTML", "CSS", "REST APIs"],
+  },
+
+  {
+    year: "2024 - 2025",
+    role: "Student",
+    company: "SoftLogic SLA",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: [
+      "ReactJs",
+      "JavaScript",
+      "NodeJs",
+      "ExpressJs",
+      "MongoDB",
+      "HTML5",
+      "CSS3",
+      "TailwindCSS",
+    ],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Trainee Engineer",
+    company: "Tech Star Pvt Ltd",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Automation",
+      "E2E-Tag Validation",
+    ],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website (ShopZilla)",
+    /*  image: project1, */
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    /* image: project2, */
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    /* image: project3, */
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "TailwindCSS"],
+  },
+  {
+    title: "Blogging Platform",
+    /* image: project4, */
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Chennai, TamilNadu",
+  phoneNo: "8754237792",
+  email: "siddhusiddarth003@gmail.com",
+};
