057d6453365adc85a2b4507afe238975c6426f7c
maga-siddarth <siddhusiddarth003@gmail.com>
2025-02-25 17:26:35 +0530

    new changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-4 ">
-      <h2 className="my-20 text-center text-4xl sm:mt-150  ">
+      <h2 className="my-20 text-center text-4xl lg:mt-20   sm:mt-150  ">
         About
         <span className="text-neutral-500"> Myself</span>
       </h2>
